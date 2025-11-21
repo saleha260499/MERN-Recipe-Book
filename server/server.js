41be26bf9b108cd527681b59e358e13073f5bcf5
Salehabanu Pathan <120989420+saleha260499@users.noreply.github.com>
2025-11-21 16:56:40 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,22 +14,17 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/recipes", recipeRoutes);
 
-
-// MongoDB connectionconst mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
-
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to the Recipe Book API 🍲');
 });
 
-// Start the serverconst PORT = process.env.PORT || 5000;
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
